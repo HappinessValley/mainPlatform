bf55791cd7e6199a77f84b150f9462e6ee5a7501
Himanshu Masant <masanthimanshu@gmail.com>
2022-06-12 14:02:20 +0530

    functions used in place of constants

--- app.js
+++ app.js
@@ -3,19 +3,10 @@ import bodyParser from "body-parser";
 import { engine } from "express-handlebars";
 import fs from "node:fs";
 
-// Handlebars Objects Starts
-
-const homeObj = JSON.parse(fs.readFileSync("./json/home.json"));
-const courseObj = JSON.parse(fs.readFileSync("./json/course.json"));
-const courseDetailObj = JSON.parse(fs.readFileSync("./json/courseDetail.json"));
-const contactObj = JSON.parse(fs.readFileSync("./json/contact.json"));
-const coachesObj = JSON.parse(fs.readFileSync("./json/coaches.json"));
-const coachDetailObj = JSON.parse(fs.readFileSync("./json/coachDetail.json"));
-const aboutObj = JSON.parse(fs.readFileSync("./json/about.json"));
-const founderObj = JSON.parse(fs.readFileSync("./json/founder.json"));
-const testimonialObj = JSON.parse(fs.readFileSync("./json/testimonial.json"));
-const workshopObj = JSON.parse(fs.readFileSync("./json/workshops.json"));
-const notFoundObj = JSON.parse(fs.readFileSync("./json/404NotFound.json"));
+// Handlebars Objects
+function readObj(file) {
+  return JSON.parse(fs.readFileSync(`./json/${file}.json`));
+}
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -36,58 +27,56 @@ app.set("view engine", "handlebars");
 // Pages Request
 
 app.get("/", (req, res) => {
-  res.render("index", homeObj);
+  res.render("index", readObj("home"));
 });
 
 app.get("/courses", (req, res) => {
-  res.render("course", courseObj);
+  res.render("course", readObj("course"));
 });
 
 app.get("/testimonials", (req, res) => {
-  res.render("testimonial", testimonialObj);
+  res.render("testimonial", readObj("testimonial"));
 });
 
 app.get("/coaches", (req, res) => {
-  res.render("coaches", coachesObj);
+  res.render("coaches", readObj("coaches"));
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", aboutObj);
+  res.render("about", readObj("about"));
 });
 
 app.get("/founder", (req, res) => {
-  res.render("founder", founderObj);
+  res.render("founder", readObj("founder"));
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact", contactObj);
+  res.render("contact", readObj("contact"));
 });
 
 // Course Details Page
 
 app.get("/courses/:id/details", (req, res) => {
   const { id } = req.params;
-  if (id > 11) {
-    res.status(404).render("404NotFound", notFoundObj);
-  }
-  res.render("courseDetail", courseDetailObj.details[id]);
+  if (id > 11) res.status(404).render("404NotFound", readObj("404NotFound"));
+  res.render("courseDetail", readObj("courseDetail").details[id]);
 });
 
 // Coach Details Page
 
 app.get("/coaches/:id/details", (req, res) => {
   const { id } = req.params;
-  res.render("coachDetail", coachDetailObj);
+  res.render("coachDetail", readObj("coachDetail"));
 });
 
 app.get("/workshops", (req, res) => {
-  res.render("workshops", workshopObj);
+  res.render("workshops", readObj("workshop"));
 });
 
 // 404 Not Found Page
 
 app.get("*", (req, res) => {
-  res.status(404).render("404NotFound", notFoundObj);
+  res.status(404).render("404NotFound", readObj("404NotFound"));
 });
 
 // Enquiry Form Route
